c44400caaa8b1272b5c228a296a5d1d278a613f0
rheamendis <mendisrhea@gmail.com>
2016-06-27 23:20:04 +0530

    form

---
+++ tutorial/main.js
@@ -0,0 +1,12 @@
+//pop up the form on button click
+$(document).ready(function(){
+    $(".container-s").hide();
+
+    $(".btn-sign").click(function () {
+        $(".container-s").show();
+    });
+
+
+
+});
+
